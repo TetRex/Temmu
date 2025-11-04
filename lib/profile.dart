f72f42b49c6b710020df3c98d00272cdc5865dc3
OTOfngn <c98423795274152963@outlook.com>
2025-11-04 14:48:20 +0200

    1

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
 import 'package:e_commerce_app/components/bottombar.dart';
 import 'package:e_commerce_app/components/snackbar.dart';
 import 'package:flutter/material.dart';
@@ -5,21 +7,22 @@ import 'package:google_fonts/google_fonts.dart';
 
 dynamic bgcolor = Colors.white;
 dynamic fgcolor = Colors.deepOrange;
-dynamic buttoncolor = Color.fromARGB(255, 100, 100, 100);
+dynamic buttoncolor = Color.fromARGB(255, 239, 239, 239);
 dynamic balance = 0.00;
 
 class SettingRow extends StatelessWidget {
   final String iconPath;
   final String text;
+  final bool bar;
 
-  const SettingRow({super.key, required this.iconPath, required this.text});
+  const SettingRow({super.key, required this.iconPath, required this.text, this.bar = true});
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Row(
               children: [
@@ -28,7 +31,7 @@ class SettingRow extends StatelessWidget {
                     elevation: 0,
                     shadowColor: bgcolor,
                     overlayColor: bgcolor,
-                    backgroundColor: Colors.white,
+                    backgroundColor: bgcolor ,
                     padding: EdgeInsets.symmetric(horizontal: 110, vertical: 20),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(0),
@@ -55,7 +58,7 @@ class SettingRow extends StatelessWidget {
                 ),
               ],
             ),
-            // SizedBox(height: 20),
+            Container(height: 2, width: 200, color: buttoncolor),
           ],
         ),
       ],
@@ -120,17 +123,21 @@ class Profile extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(10),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: [
-                            ImageIcon(
-                              AssetImage('assets/icons/user.png'),
-                              size: 120,
-                            ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      
+                      color: buttoncolor,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              ImageIcon(
+                                AssetImage('assets/icons/user.png'),
+                                size: 120,
+                              ),
                             // Text(
                             //   'My.Name',
                             //   style: GoogleFonts.interTight(
@@ -138,36 +145,34 @@ class Profile extends StatelessWidget {
                             //     color: Colors.black
                             //   ),
                             // )
-                          ],
-                        ),
-                        SizedBox(width: 50,),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              balance.toString(),
-                              style: GoogleFonts.interTight(
-                                fontSize: 60,
-                                color: fgcolor,
-                                fontWeight: FontWeight.bold,
+                            ],
+                          ),
+                          SizedBox(width: 50),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                balance.toString(),
+                                style: GoogleFonts.interTight(
+                                  fontSize: 60,
+                                  color: fgcolor,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Text(
-                              'My Balance',
-                              style: GoogleFonts.interTight(
-                                fontSize: 30,
-                                color: Colors.black,
+                              Text(
+                                'My Balance',
+                                style: GoogleFonts.interTight(
+                                  fontSize: 30,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ],
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 50,),
-                    SettingRow(
-                      iconPath: 'assets/icons/user.png',
-                      text: 'My Orders',
-                    ),
+                    SizedBox(height: 20),
+                    SettingRow(iconPath: 'assets/icons/user.png', text: 'My Orders'),
                     SettingRow(iconPath: 'assets/icons/contrast.png', text: 'Theme'),
                     SettingRow(iconPath: 'assets/icons/messages.png', text: 'Messages'),
                     SettingRow(iconPath: 'assets/icons/language.png', text: 'Language'),

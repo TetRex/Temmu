ea120f56e8cb21f42d9386dbe199d1f3275cce89
TetRex <tetrex228@pm.me>
2025-11-04 15:21:44 +0200

    update: refactor SettingRow component for improved layout and remove unused parameters

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
-
 import 'package:e_commerce_app/components/bottombar.dart';
 import 'package:e_commerce_app/components/snackbar.dart';
 import 'package:flutter/material.dart';
@@ -7,64 +5,42 @@ import 'package:google_fonts/google_fonts.dart';
 
 dynamic bgcolor = Colors.white;
 dynamic fgcolor = Colors.deepOrange;
-dynamic buttoncolor = Color.fromARGB(255, 239, 239, 239);
+dynamic buttoncolor = Color.fromARGB(255, 100, 100, 100);
 dynamic balance = 0.00;
 
 class SettingRow extends StatelessWidget {
   final String iconPath;
   final String text;
-  final bool bar;
 
-  const SettingRow({super.key, required this.iconPath, required this.text, this.bar = true});
+  const SettingRow({super.key, required this.iconPath, required this.text});
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 0,
+          shadowColor: bgcolor,
+          overlayColor: bgcolor,
+          backgroundColor: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
+        ),
+        onPressed: () => showNotReadySnackBar(context),
+        child: Row(
+          mainAxisSize: MainAxisSize.min, // Important: Don't expand full width
           children: [
-            Row(
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    shadowColor: bgcolor,
-                    overlayColor: bgcolor,
-                    backgroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 110,
-                      vertical: 20,
-                    ), 
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0),
-                    ),
-                  ),
-                  onPressed: () => showNotReadySnackBar(context),
-                  child: Row(
-                    children: [
-                      ImageIcon(
-                        AssetImage(iconPath),
-                        size: 30,
-                        color: Colors.black,
-                      ),
-                      SizedBox(width: 10),
-                      Text(
-                        text,
-                        style: GoogleFonts.interTight(
-                          color: Colors.black,
-                          fontSize: 30,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+            ImageIcon(AssetImage(iconPath), size: 30, color: Colors.black),
+            SizedBox(width: 10),
+            Text(
+              text,
+              style: GoogleFonts.interTight(color: Colors.black, fontSize: 30),
             ),
-            Container(height: 2, width: 200, color: buttoncolor),
           ],
         ),
-      ],
+      ),
     );
   }
 }
@@ -119,73 +95,90 @@ class Profile extends StatelessWidget {
           // ],
         ),
 
-        body: Container(
-          margin: const EdgeInsets.only(left: 0, right: 0),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      
-                      color: buttoncolor,
-                      child: Row(
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          ImageIcon(
+                            AssetImage('assets/icons/user.png'),
+                            size: 120,
+                          ),
+                          // Text(
+                          //   'My.Name',
+                          //   style: GoogleFonts.interTight(
+                          //     fontSize: 30,
+                          //     color: Colors.black
+                          //   ),
+                          // )
+                        ],
+                      ),
+                      SizedBox(width: 50),
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Column(
-                            children: [
-                              ImageIcon(
-                                AssetImage('assets/icons/user.png'),
-                                size: 120,
-                              ),
-                            // Text(
-                            //   'My.Name',
-                            //   style: GoogleFonts.interTight(
-                            //     fontSize: 30,
-                            //     color: Colors.black
-                            //   ),
-                            // )
-                            ],
+                          Text(
+                            balance.toString(),
+                            style: GoogleFonts.interTight(
+                              fontSize: 60,
+                              color: fgcolor,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          SizedBox(width: 50),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                balance.toString(),
-                                style: GoogleFonts.interTight(
-                                  fontSize: 60,
-                                  color: fgcolor,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                'My Balance',
-                                style: GoogleFonts.interTight(
-                                  fontSize: 30,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
+                          Text(
+                            'My Balance',
+                            style: GoogleFonts.interTight(
+                              fontSize: 30,
+                              color: Colors.black,
+                            ),
                           ),
                         ],
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    SettingRow(iconPath: 'assets/icons/user.png', text: 'My Orders'),
-                    SettingRow(iconPath: 'assets/icons/contrast.png', text: 'Theme'),
-                    SettingRow(iconPath: 'assets/icons/messages.png', text: 'Messages'),
-                    SettingRow(iconPath: 'assets/icons/language.png', text: 'Language'),
-                    SettingRow(iconPath: 'assets/icons/settings.png', text: 'settings'),
-                    SettingRow(iconPath: 'assets/icons/info.png', text: 'About')
-                  ],
-                ),
+                    ],
+                  ),
+
+                  SizedBox(height: 50),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SettingRow(
+                        iconPath: 'assets/icons/user.png',
+                        text: 'My Orders',
+                      ),
+                      SettingRow(
+                        iconPath: 'assets/icons/contrast.png',
+                        text: 'Theme',
+                      ),
+                      SettingRow(
+                        iconPath: 'assets/icons/messages.png',
+                        text: 'Messages',
+                      ),
+                      SettingRow(
+                        iconPath: 'assets/icons/language.png',
+                        text: 'Language',
+                      ),
+                      SettingRow(
+                        iconPath: 'assets/icons/settings.png',
+                        text: 'settings',
+                      ),
+                      SettingRow(
+                        iconPath: 'assets/icons/info.png',
+                        text: 'About',
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
 
         bottomNavigationBar: BottomBar(currentPage: 'profile'),

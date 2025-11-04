8ef3d91bcbed08f2c647435c42f2645d353cd9c3
TetRex <tetrex228@pm.me>
2025-11-04 10:47:50 +0200

    update: add SettingRow component for user orders and integrate snackbar functionality

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/components/bottombar.dart';
+import 'package:e_commerce_app/components/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,6 +7,52 @@ dynamic bgcolor = Colors.white;
 dynamic fgcolor = Colors.deepOrange;
 dynamic buttoncolor = Color.fromARGB(255, 100, 100, 100);
 dynamic balance = 0.00;
+
+class SettingRow extends StatelessWidget {
+  final String iconPath;
+  final String text;
+
+  const SettingRow({super.key, required this.iconPath, required this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    shadowColor: Colors.white,
+                    overlayColor: Colors.white,
+                    backgroundColor: Colors.white,
+                  ),
+                  onPressed: () => showNotReadySnackBar(context),
+                  child: Row(
+                    children: [
+                      ImageIcon(AssetImage(iconPath)),
+                      Text(
+                        text,
+                        style: GoogleFonts.interTight(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ],
+    );
+  }
+}
 
 // class text extends Text
 void main() {
@@ -73,7 +120,7 @@ class Profile extends StatelessWidget {
                           children: [
                             ImageIcon(
                               AssetImage('assets/icons/user.png'),
-                              size: 120
+                              size: 120,
                             ),
                             // Text(
                             //   'My.Name',
@@ -82,7 +129,7 @@ class Profile extends StatelessWidget {
                             //     color: Colors.black
                             //   ),
                             // )
-                          ]
+                          ],
                         ),
                         Text("        "),
                         Column(
@@ -94,94 +141,25 @@ class Profile extends StatelessWidget {
                                 fontSize: 60,
                                 color: fgcolor,
                                 fontWeight: FontWeight.bold,
-                              )
+                              ),
                             ),
                             Text(
                               'My Balance',
                               style: GoogleFonts.interTight(
                                 fontSize: 30,
-                                color: Colors.black
-                              )
-                            )
-                          ]
-                        )
-                      ]
-                    ),
-                    Text(''),
-                    Text(''),
-                    Text(''),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ImageIcon(AssetImage('assets/icons/user.png'), size: 30),
-                        Text(
-                          ' My Orders',
-                            style: GoogleFonts.interTight(
-                            fontSize: 30,
-                            color: Colors.black
-                          ),
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                     Text(''),
                     Text(''),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ImageIcon(AssetImage('assets/icons/contrast.png'), size: 30),
-                        Text(
-                          ' Theme',
-                            style: GoogleFonts.interTight(
-                            fontSize: 30,
-                            color: Colors.black
-                          ),
-                        ),
-                      ],
-                    ),
                     Text(''),
-                    Text(''),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ImageIcon(AssetImage('assets/icons/messages.png'), size: 30),
-                        Text(
-                          ' Messages',
-                            style: GoogleFonts.interTight(
-                            fontSize: 30,
-                            color: Colors.black
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(''),
-                    Text(''),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ImageIcon(AssetImage('assets/icons/language.png'), size: 30),
-                        Text(
-                          ' Language',
-                            style: GoogleFonts.interTight(
-                            fontSize: 30,
-                            color: Colors.black
-                          ),
-                        ),
-                      ],
-                    ),
-                    Text(''),
-                    Text(''),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ImageIcon(AssetImage('assets/icons/settings.png'), size: 30),
-                        Text(
-                          ' Settings',
-                            style: GoogleFonts.interTight(
-                            fontSize: 30,
-                            color: Colors.black
-                          ),
-                        ),
-                      ],
+                    SettingRow(
+                      iconPath: 'assets/icons/user.png',
+                      text: 'My Orders',
                     ),
                   ],
                 ),
